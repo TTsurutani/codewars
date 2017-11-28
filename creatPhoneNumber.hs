900338a220d68f530f9413f610eb90c605f29ea5
tbird <tbird@jp.ibm.com>
2017-11-29 08:48:06 +0900

    完成

---
+++ creatPhoneNumber.hs
@@ -0,0 +1,7 @@
+module CreatePhoneNumber where
+import Data.Char    
+
+createPhoneNumber :: [Int] -> String
+createPhoneNumber xs = "(" ++ take 3 list ++ ") " ++ take 3 (drop 3 list) ++ "-" ++ drop 6 list
+                            where 
+                                 list = map intToDigit xs
