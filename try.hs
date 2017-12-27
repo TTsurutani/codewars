ae08f0a0af2e7552d95d728c26e0505738d5f799
tbird <tbird@jp.ibm.com>
2017-12-27 16:52:48 +0900

    進捗

--- try.hs
+++ try.hs
@@ -1,16 +1,20 @@
 import Data.List
-import Data.Char
+relativelyPrime :: Integral t => t -> [t] -> [t]
+relativelyPrime 0 [0] = []
+relativelyPrime n l = filter' (list n) l
 
--- validateHello :: String -> Bool
--- validateHello cs@(x:_) = any x ["hello","ciao","salut","hallo","hola","ahoj","czesc"]
-songDecoder :: String -> String
-songDecoder xs = lastspace $ headspace $ sep xs
+--篩による素数の無限リスト
+sieve :: Integral a => [a] -> [a]
+sieve (x:xs) = x : sieve (filter (\z -> z `mod` x /= 0) xs)
+--素数リスト
+primes :: Integral a => a -> [a]
+primes n = takeWhile (< n) $ sieve [2..] 
+--素数リストから約数のリストを作る
+--nを順に素数で割っていきmod=0のみの残す
+list :: Integral a => a -> [a]
+list n = filter (\x -> n `mod` x == 0) $ primes n                   
+--約数のリストで対象リストを順次割って余りが出るものを残す
+filter' :: Integral a => [a] -> [a] -> [a]
+filter' [] ls = ls
+filter' (x:xs) ls = filter' xs $ filter (\y -> y `mod` x /= 0) ls
 
-sep :: String -> String
-sep ('W':'U':'B':xs) = ' ' : sep xs
-sep [] = []
-sep (x:xs) = x : sep xs
-
-headspace xs = if head xs == ' ' then tail xs else xs
-
-lastspace xs = if last xs == ' ' then init xs else xs
