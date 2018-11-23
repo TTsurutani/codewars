effa12cf0b5483c67e25e27e558002457e591dea
TTsurutani <tbird@mbj.nifty.com>
2018-11-24 08:16:19 +0900

    test2

--- try2.hs
+++ try2.hs
@@ -1,3 +1,5 @@
+import Data.Char (digitToInt, toLower)
+
 sieve (x:xs) = x : sieve (filter (\y -> y `mod` x /= 0) xs)
 primes = sieve [2..]
 
@@ -19,3 +21,18 @@ elementAt n ls@(x:xs)
   | n > length ls = Nothing
   | n == 1 = Just x
   | otherwise = elementAt (n - 1) xs
+
+squareDigit num = (read::String->Int) $ concatMap (show . (^ 2) . digitToInt) (show num)
+-- squareDigit num = (read::String->Int) $ concat $ map show $ map (^2) $  map digitToInt  $ show num
+
+highAndLow :: String -> String
+highAndLow input = show (maximum list) ++ " " ++ show (minimum list)
+  where list = words input
+ 
+
+xo :: String -> Bool
+xo str = a == b
+ where 
+     str' = map toLower str
+     a = length $ filter (== 'x') str
+     b = length $ filter (== 'o') str  
